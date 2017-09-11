79813dc1b2df82f78c5a0b5500b07a79cd0bc592
Thomas, Catherine <thomcat@amazon.com>
2017-09-11 16:10:31 -0700

    initial commit

---
+++ MyCalculator/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by Thomas, Catherine on 8/23/17.
+//  Copyright © 2017 FOCAL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func hitNumber(_ sender: UIButton) {
+        
+        
+    }
+}
+
