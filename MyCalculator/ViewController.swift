e352518bfda58923248f63ca99e715e7bc4e8b26
Thomas, Catherine <thomcat@amazon.com>
2018-01-11 09:16:43 -0800

    final working calculator

--- MyCalculator/ViewController.swift
+++ MyCalculator/ViewController.swift
@@ -9,14 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var numberLabel: UILabel!
+    var currentOperation: String? = nil
+    var mathString: String = ""
+    
+    // clear numbers function
+    @IBAction func hitClear(){
+        numberLabel.text = "0"
+        currentOperation = nil
+        mathString = "" 
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // change sign function
+    @IBAction func hitChangeSign() {
+        let number = Double(numberLabel.text ?? "0") ?? 0
+        numberLabel.text = "\(-number)"
+    }
+    
+    // percentage function
+    @IBAction func hitPercentage() {
+        // divides the number by 100
+        let number = Double(numberLabel.text ?? "0") ?? 0
+        numberLabel.text = "\(number/100)"
+    }
+    
+    @IBAction func hitNumber(_ sender: UIButton) {
+        if let number = sender.titleLabel?.text {
+            if let original = numberLabel.text, original != "0" && currentOperation == nil {
+                numberLabel.text?.append(number)
+            } else {
+                if let op = currentOperation {
+                    mathString.append(numberLabel.text ?? "")
+                    
+                    if op == "x" {
+                        mathString.append("*")
+                    } else {
+                        mathString.append(op)
+                    }
+                    
+                    currentOperation = nil
+                }
+                
+                numberLabel.text = number
+            }
+        }
+    }
+    
+    @IBAction func hitOperation(_ sender: UIButton) {
+        currentOperation = sender.titleLabel?.text
+    }
+    
+    @IBAction func hitDecimal() {
+        if let text = numberLabel.text, !text.contains(".") {
+            numberLabel.text?.append(".")
+        }
+    }
+    
+    // equals function
+    @IBAction func hitEquals() {
+        mathString.append(numberLabel.text ?? "")
+        let exp = NSExpression(format: mathString)
+        if let result = exp.expressionValue(with: nil, context: nil) as? Double {
+            numberLabel.text = "\(result)"
+        }
+        mathString = ""
     }
 }
 
